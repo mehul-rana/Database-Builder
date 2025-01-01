bdd31365bc58644523e4ace6cfeccbfc3c496000
Mehul Rana <mehulrana412@gmail.com>
2025-01-01 14:41:28 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = 8005;
+require('dotenv').config();
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'tracker'
+
+MongoClient.connect(dbConnectionStr)
+  .then(client => {
+    console.log(`Connected to ${dbName} Database`)
+    db = client.db(dbName)
+  });
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(process.env.PORT || PORT, () =>{
+  console.log(`Server running on port ${PORT}`)
+})
